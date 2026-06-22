06e919cd2536668f5bc179f665509ae16a3111fa
felps-bretas <felipebretasprata@gmail.com>
2026-06-22 18:50:27 -0300

    a

--- rdocs/source/packages.R
+++ rdocs/source/packages.R
@@ -14,41 +14,6 @@ pacman::p_load(
 options(scipen=999)
 options(OutDec = ",")
 
-# Definindo paleta de cores da Estat
-cores_estat <- c(
-  "#A11D21", "#003366", "#CC9900", "#663333", "#FF6600",
-  "#CC9966", "#999966", "#006606", "#008091", "#041835",
-  "#666666")
-
-theme_estat <- function(...) {
-  theme <- ggplot2::theme_bw() +
-    ggplot2::theme(
-      axis.title.y = ggplot2::element_text(colour = "black", size = 10),
-      axis.title.x = ggplot2::element_text(colour = "black", size = 10),
-      axis.text = ggplot2::element_text(colour = "black", size = 10),
-      panel.border = ggplot2::element_blank(),
-      axis.line = ggplot2::element_line(colour = "black"),
-      text = element_text(family = "sans", size = 12),
-      legend.position = "top",
-      ...
-    )
-  return(
-    list(
-      theme,
-      scale_fill_manual(values = cores_estat),
-      scale_colour_manual(values = cores_estat),
-      scale_x_continuous(
-        labels = scales::number_format(decimal.mark = ',',
-                                       #accuracy = 0.01,
-                                       big.mark = ".")),
-      scale_y_continuous(
-        labels = scales::number_format(decimal.mark = ',',
-                                       #accuracy = 0.01,
-                                       big.mark = "."))
-    )
-  )
-}
-
 # Definindo função que retorna frequências relativas de um vetor
 percent <- function(absolute, digits = 2) {
   return(round(100 * absolute / sum(absolute), digits))
